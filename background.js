24fedc5f5d6b9fb9e62918828702436088eb291a
sburba <github@samburba.com>
2016-07-22 14:45:56 -0400

    Initial commit

---
+++ background.js
@@ -0,0 +1,39 @@
+chrome.browserAction.onClicked.addListener(loadCommentsForTab);
+
+const URL_SEARCH_PREFIX = 'https://www.reddit.com/search.json?sort=comments&q=url:';
+
+/**
+ *
+ * @param {Tab} tab
+ */
+function loadCommentsForTab(tab) {
+    doSearchForUrl(tab.url)
+        .then(getTopLink)
+        .then(makeTab)
+        .catch(function() {
+            console.log('no comments found');
+        });
+}
+
+function doSearchForUrl(url) {
+    let search = URL_SEARCH_PREFIX + url;
+    return window.fetch(search)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Unable to make request');
+            }
+        });
+}
+
+function getTopLink(searchJson) {
+    return 'https://reddit.com' + searchJson.data.children[0].data.permalink;
+}
+
+function makeTab(url) {
+    chrome.tabs.create({
+        url: url,
+        active: true
+    });
+}
